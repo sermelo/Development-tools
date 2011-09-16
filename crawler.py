6ecd57340baa8828a0e6d6708469839e43a6f52d
Sergio <sergio.merino.a@gmail.com>
2011-09-16 20:40:33 +0200

    Now it add the complete url to relative urls

--- crawler.py
+++ crawler.py
@@ -7,19 +7,31 @@ import argparse
 urlweb="http://www.wired.com/"
 
 def read_web(urlweb,n):
-    _opener = urllib2.build_opener()
-    try:
-        raw_code = _opener.open(urlweb).read()
-    except:
-        print "We could not open this url:"+urlweb
-        return
-    n=n-1
-    soup_code = Soup ( raw_code )
-    for link in soup_code.findAll('a'):
-        if link.has_key("href"):
-            print link["href"]
-            if n!=0:
-                read_web(link['href'],n)
+    if n==0:
+        print urlweb
+    else:
+        _opener = urllib2.build_opener()
+        try:
+            raw_code = _opener.open(urlweb).read()
+        except:
+            print "We could not open this url:"+urlweb
+            return
+        soup_code = Soup ( raw_code )
+        n=n-1
+        for link in soup_code.findAll('a'):
+            if link.has_key("href"):
+               # print link["href"]
+                if n>=0:
+                    auxlink=str(link['href'])
+                    auxlink=auxlink.strip()
+                    if auxlink=="":
+                        continue
+                    elif auxlink[0]=="/":
+                        read_web(urlweb+link['href'],n)
+                    elif auxlink[0]=="#":
+                        continue
+                    else:
+                        read_web(auxlink,n)
             
 
 parser=argparse.ArgumentParser(description="This is a crawler")
