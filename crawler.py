c9583704b26437f8d4b12b2ec0d5f3f7b9b8c243
Sergio <sergio.merino.a@gmail.com>
2011-09-22 20:02:19 +0200

    Added mozilla user agent

--- crawler.py
+++ crawler.py
@@ -38,9 +38,13 @@ def read_web(urlweb,n,total_levels):
 
     complete_list[total_levels-n].append(urlweb)
     if n!=0:
-        _opener = urllib2.build_opener()
+
+        user_agent = "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari /534.7 "
+        opener = urllib2.build_opener()
+        opener.addheaders=[('User-agent',user_agent)]
+
         try:
-            raw_code = _opener.open(urlweb,"",4).read()
+            raw_code = opener.open(urlweb,"",4).read()
         except:
             print "\nWe could not open this url:"+urlweb
             return
